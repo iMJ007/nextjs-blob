df776a948542eb03d224fdbec729e83182f71c95
iMJ007 <mohammed.junaid1000@gmail.com>
2022-06-05 17:23:53 +0530

    dynamic routing

---
+++ pages/index.jsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
+  return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>
+					Hello, I am Junaid! I'm a budding web and android software engineer!
+					You can contact me on{" "}
+					<a href="https://twitter.com/MohammedJunaidG" target="_blank">
+						Twitter
+					</a>
+				</p>
+				<p>
+					(This is a sample website - you’ll be building a site like this on{" "}
+					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+				</p>
+			</section>
+
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
+}
